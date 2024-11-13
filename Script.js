c56a6b7e1fe57793cc57343c87655c5758d5b413
Parth Shrivastava <139749696+Vishwas567917@users.noreply.github.com>
2024-11-13 22:05:29 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,26 @@
+const apiKey = 'NASA_API_KEY';
 
+async function fetchData() {
+  try {
+    const response = await fetch(https://api.nasa.gov/planetary/apod?api_key=${1Xg2FffmZHikbXqgFi5e1eG3F42Q7QBvnOHk6Udd});
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    const data = await response.json();
+    showDataOnUI(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+function showDataOnUI(data) {
+  const imgContainer = document.getElementById("image-container");
+  let img = document.createElement("img");
+  img.src = data.hdurl;
+  img.height = 500;
+  img.width = 500;
+  img.alt = data.title;
+  imgContainer.appendChild(img);
+}
+
+fetchData();
