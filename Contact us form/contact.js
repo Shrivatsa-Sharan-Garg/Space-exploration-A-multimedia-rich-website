2bb1cd3eee09b880dcdab9a5f699bc68d1732033
Sudhanshu-Ambastha <sudhanshuambastha2020@gmail.com>
2024-11-23 17:36:08 +0530

    ⬆update

--- Contact us form/contact.js
+++ Contact us form/contact.js
@@ -1,4 +1,10 @@
-const btn = document.querySelector("button");
-        btn.addEventListener("click", () => {
-            alert("Your message has been sent!");
-        });
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", (event) => {
+    if (!form.checkValidity()) {
+        event.preventDefault();
+        alert("Please fill in all required fields correctly.");
+    } else {
+        alert("Your message has been sent!");
+    }
+});
